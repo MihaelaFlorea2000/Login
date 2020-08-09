7a4836afbb8345b7b7b5270a39c7cb617f5a49f6
cortana <mf2u19@soton.ac.uk>
2020-08-09 15:57:20 +0300

    Add Welcome screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
+import Welcome from "./Welcome";
 
-let isLoggedIn = false;
+let userIsRegistered = true;
 
-const currentTime = new Date(2020, 1, 1, 21).getHours();
+// function App() {
+//   return (
+//     <div className="container">{
+//       <Form 
+//         isRegistered={userIsRegistered}
+//       />
+//     }</div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="container">{
-      //isLoggedIn ? <h1>Hello</h1> : <Login />
-      
-      currentTime > 12 && <h1> Why are you still working? </h1>
-    }</div>
+    <Welcome />
   );
 }
 
