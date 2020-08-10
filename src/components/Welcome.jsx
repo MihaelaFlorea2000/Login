953cc916cd967c2ec8e9e8b81d679e822b486bdc
cortana <mf2u19@soton.ac.uk>
2020-08-10 15:47:04 +0300

    Add last name

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,40 +3,75 @@ import Input from "./Input";
 
 function Welcome() {
 
-  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
   const [isMouseOver, setMouseOver] = useState(false);
 
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-
-  function handleMouseOut() {
-    setMouseOver(false);
-  }
-
   function handleChange(event) {
-    console.log(event.target.value);
-    setName(event.target.value);
-  }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+    const {value, name} = event.target;
+    // const newValue = event.target.value;
+    // const inputName = event.target.name;
+
+    setFullName((prevValue) => {
+        if (name === "fName") {
+          return {
+            fName: value,
+            lName: prevValue.lName
+          }
+        } else {
+          return {
+            fName: prevValue.fName,
+            lName: value
+          }
+        }
+    });
+
+    // if (inputName === "fName") {
+    //   setFullName({
+    //     fName: newValue,
+    //     lName: fullName.lName
+    //   });
+    // } else {
+    //   setFullName({
+    //     fName: fullName.fName,
+    //     lName: newValue
+    //   });
+    // }
   }
 
   return (
     <form onSubmit={(event) => {event.preventDefault()}}>
-      <h1>Hello {name}</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <input 
+        name="fName"
         onChange={handleChange} 
         type="text" 
-        placeholder="What's your name?"
-        value={name}
+        placeholder="What's your first name?"
+        value={fullName.fName}
+      />
+      <input
+        name = "lName"
+        onChange={handleChange} 
+        type="text" 
+        placeholder="What's your last name?"
+        value={fullName.lName}
       />
       <button
         type="submit"
         style={{backgroundColor: isMouseOver ? "black" : "white"}}
-        onMouseOver={handleMouseOver} 
-        onMouseOut={handleMouseOut}
+        onMouseOver = {
+          () => {
+            setMouseOver(true);
+          }
+        }
+        onMouseOut = {
+          () => {
+            setMouseOver(false);
+          }
+        }
       >Submit</button>
     </form>
   );
